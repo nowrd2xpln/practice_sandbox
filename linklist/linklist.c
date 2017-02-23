68066a1b33d625f035f07e7b25bc2bdef72287f1
Allen Mamaril <allen.mamaril@gmail.com>
2017-02-22 23:17:03 -0800

    Implement init/delete list functons

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node Node;
 typedef struct list List;
@@ -13,12 +14,32 @@ struct
 {
     int cnt;
     Node *head;
-    Node *tail
+    Node *tail;
 } list;
+
+List *initList(void)
+{
+    List *llist = NULL;
+
+    llist = (List *) malloc(sizeof(list));
+
+    return llist;
+}
+
+int deleteList(List *llist)
+{
+    free(llist);
+
+    return 0;
+}
 
 int insertNode(List *llist, int data)
 {
+    return 0;
+}
 
+void traverseList(List *llist)
+{
     return 0;
 }
 
