b428199e290ee252c532549260760e85200e3cb7
Allen Mamaril <allen.mamaril@gmail.com>
2017-02-22 22:31:08 -0800

    Create node and list structures

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+typedef struct node Node;
+typedef struct list List;
+
+struct
+{
+    int cnt;
+    Node *next;
+} node;
+
+struct
+{
+    int num;
+    Node *head;
+    Node *tail
+} list;
+
 int main(void)
 {
     printf("Link List Review\n");
