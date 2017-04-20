48ed4270c6eaa73d922f4e9ffcad09610caf21c7
nowrd2xpln <allen.mamaril@gmail.com>
2017-04-19 21:47:01 -0700

    Perform some cleaning and reformatting. Implement init, create, & delete API. Builds successfully

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -22,48 +22,60 @@ List *initList(void)
     List *llist = NULL;
 
     llist = (List *) malloc(sizeof(List));
+    llist->head = NULL;
+    llist->tail = NULL;
+    llist->cnt = 0;
 
     return llist;
 }
 
 void deleteList(List *llist)
 {
-    Node *ptr = NULL;
-    Node *tmp = NULL;
+	Node *ptr = NULL;
+	Node *tmp = NULL;
 
-    if(llist == NULL)
-    {
-        ptr = llist->head;
-        llist->head = NULL;
+	printf("%s: enter\n", __FUNCTION__);
+
+	if(llist != NULL) {		
+ 		ptr = llist->head;
+ 		llist->head = NULL;
         
-        while(ptr != NULL)
-        {
-            tmp = ptr->next;
-            free(ptr);
-            ptr = tmp;
-        }
-    }
+ 		while(ptr != NULL) {
+ 			tmp = ptr->next;
+			printf("DeleteNode(%d): %p - %d\n", llist->cnt, ptr, ptr->data);
+
+ 			free(ptr);
+			llist->cnt--;
+
+            		ptr = tmp;
+		}
+	}
+	
 }
 
 int insertNode(List *llist, int value)
 {
-    Node *newnode = NULL;
-    int ret = 0;
+    	Node *newnode = NULL;
+    	int ret = 0;
 
-    // Create new node
-    newnode = (Node *) malloc(sizeof(Node));
+    	// Create new node
+    	newnode = (Node *) malloc(sizeof(Node));
 
-    if(newnode == NULL)
-    {
-        printf("Failed to allocate memory for new node\n");
-        ret = -1;
-    }
-    else
-    {
-        newnode->data = value;
-    }
+	if(newnode == NULL) {
+		printf("Failed to allocate memory for new node\n");
+		ret = -1;
+	}
+	else {
+		newnode->data = value;
+	}
 
-    return ret;
+	newnode->next = llist->head;
+	llist->head = newnode;
+	llist->cnt++;
+
+	printf("NewNode(%d): %p - %d\n", llist->cnt, llist->head, llist->head->data);
+
+	return ret;
 }
 
 void traverseList(List *llist)
@@ -73,7 +85,12 @@ void traverseList(List *llist)
 
 int main(void)
 {
-    printf("Link List Review\n");
+	List *L1 = NULL;
 
+	printf("Link List Review\n");
+
+	L1 = initList();
+	insertNode(L1, 57);
+	deleteList(L1);
     return 0;
 }
