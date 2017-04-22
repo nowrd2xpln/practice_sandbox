def0559a2921d9945c2aa476b8c2fae69bafe817
nowrd2xpln <allen.mamaril@gmail.com>
2017-04-22 11:21:30 -0700

    Implement deleteFromList() and test cases

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -109,6 +109,47 @@ void searchList(List *llist, int data)
         cur = cur->next;
     }
 }
+
+void deleteFromList(List *llist, int data)
+{
+    printf("%s: enter\n", __FUNCTION__);
+    Node *cur = NULL;
+    int idx =  0;
+
+    if(llist->head == NULL) // Empty list
+        return;
+
+    idx = llist->cnt;
+    cur = llist->head;
+
+    if(cur->data == data) { // Match is found in first node
+        printf("Match is found in first node\n");
+        printf("Found Node(%d): %p - %d\n", idx, cur, cur->data);
+                 
+        llist->head = cur->next;
+        free(cur);
+        cur = NULL;
+    }
+
+    printf("enter while\n");
+    while(cur != NULL) {
+        if(cur->next->data == data) {
+            printf("Found Node(%d): %p - %d\n", idx + 1, cur->next, cur->next->data);
+            Node *tmp = cur->next;
+            
+            cur->next = tmp->next;
+            free(tmp);
+            tmp = NULL;
+            llist->cnt--;
+            break;
+        }
+
+        idx--;
+        cur = cur->next;
+    }
+    printf("%s: exit\n", __FUNCTION__);
+}
+
 int main(void)
 {
 	List *L1 = NULL;
@@ -116,6 +157,36 @@ int main(void)
 	printf("Link List Review\n");
 
 	L1 = initList();
+
+    // Empty list
+	printf("Empty list\n");
+    traverseList(L1);
+    searchList(L1, 57);
+    deleteFromList(L1, 57);
+    traverseList(L1);
+
+    // 1 node
+	printf("1 node\n");
+	insertNode(L1, 57);
+    traverseList(L1);
+    searchList(L1, 44);
+    searchList(L1, 57);
+    deleteFromList(L1, 57);
+    traverseList(L1);
+
+    // 2 node
+	printf("2 node\n");
+	insertNode(L1, 57);
+	insertNode(L1, 58);
+    traverseList(L1);
+    searchList(L1, 44);
+    searchList(L1, 57);
+    deleteFromList(L1, 58);
+    traverseList(L1);
+
+
+    // 3 node
+
 	insertNode(L1, 57);
 	insertNode(L1, 58);
 	insertNode(L1, 59);
@@ -125,6 +196,9 @@ int main(void)
     searchList(L1, 57);
     searchList(L1, 58);
     searchList(L1, 59);
+    traverseList(L1);
+
+    deleteFromList(L1, 58);
     traverseList(L1);
 
 	deleteList(L1);
