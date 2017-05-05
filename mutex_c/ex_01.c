ed1f7b63d3768663b5f93090426d6d5eb8212907
nowrd2xpln <allen.mamaril@gmail.com>
2017-05-04 23:24:54 -0700

    add mutex lock

--- mutex_c/ex_01.c
+++ mutex_c/ex_01.c
@@ -6,15 +6,21 @@
 
 pthread_t tid[2];
 int counter;
+pthread_mutex_t lock;
 
 void* doSomeThing(void *arg)
 {
+    pthread_mutex_lock(&lock);
+
     unsigned long i = 0;
     counter += 1;
     printf("\n Job %d started\n", counter);
 
     for(i=0; i<(0xFFFFFFFF);i++);
+    
     printf("\n Job %d finished\n", counter);
+
+    pthread_mutex_unlock(&lock);
 
     return NULL;
 }
@@ -24,8 +30,12 @@ int main(void)
     int i = 0;
     int err;
 
-    while(i < 2)
-    {
+    if(pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init failed\n\n");
+        return 1;
+    }
+
+    while(i < 2) {
         err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
         if (err != 0)
             printf("\ncan't create thread :[%s]", strerror(err));
@@ -34,6 +44,7 @@ int main(void)
 
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
+    pthread_mutex_destroy(&lock);
 
     return 0;
 }
