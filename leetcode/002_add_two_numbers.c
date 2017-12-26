91a299344d0bfc6556cc5073a46d31be911e4494
Allen Mamaril <allen.mamaril@gmail.com>
2017-12-26 15:11:17 -0800

    insert template

--- leetcode/002_add_two_numbers.c
+++ leetcode/002_add_two_numbers.c
@@ -1,1 +1,33 @@
-hello
+#include <stdio.h>
+#include <string.h>
+
+/** @brief Takes a string and reverses the order of
+ *         characters.
+ *
+ *  @param s The input string to be reversed
+ *         row will be written.
+ *
+ *  @return void.
+ */
+void reverseString(char* s) {
+    char *fwd = s;
+    char *bwd = s + strlen(s) - 1;;
+    
+    for(;fwd < bwd; fwd++, bwd--) {
+        // Swap
+        *fwd ^= *bwd;
+        *bwd ^= *fwd;
+        *fwd ^= *bwd;
+    }
+}
+
+int main(void)
+{
+  char input[] = "this is my string";
+  
+  printf("Before: %s\n", input);
+  reverseString(input);
+  printf("After : %s\n", input);
+  
+  return 0;
+}
