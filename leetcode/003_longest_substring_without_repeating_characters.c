3a7d98b63716f4723cf39dd25bc645f41b5a9277
nowrd2xpln <allen.mamaril@gmail.com>
2018-02-03 18:01:18 -0800

    Start leetcode problem 003

---
+++ leetcode/003_longest_substring_without_repeating_characters.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+int lefthOfLongestSubstring(char *str);
+int allUnique(char *str, int start, int end);
+
+uint32_t test01();
+uint32_t test02();
+uint32_t test03();
+
+int main(void)
+{
+    int ret = 0;
+    
+    
+    test01();
+
+    return ret;
+}
+
+int lefthOfLongestSubstring(char *str)
+{
+    uint32_t len = 0;
+
+    return len;
+}
+
+int allUnique(char *str, int start, int end)
+{
+    uint32_t ret = 0;
+
+    return ret;
+}
+
+uint32_t test01()
+{
+    uint32_t ret = 0;
+
+    
+    return ret;
+}
