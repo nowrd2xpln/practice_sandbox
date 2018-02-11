4a9f0d8330d89667133d5969e60f598525a05d4a
nowrd2xpln <allen.mamaril@gmail.com>
2018-02-10 17:39:10 -0800

    Add stdio library

---
+++ leetcode/template.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+uint32_t test01();
+uint32_t test02();
+uint32_t test03();
+
+int main(void)
+{
+    int ret = 0;
+    
+    
+    test01();
+
+    return ret;
+}
+
+uint32_t test01()
+{
+    uint32_t ret = 0;
+
+    
+    return ret;
+}
