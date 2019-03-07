d19d8a415c49c51529d760fb64baceb0d43ebe3b
nowrd2xpln <allen.mamaril@gmail.com>
2019-03-06 20:27:09 -0800

    Create problem 5

---
+++ leetcode/005_longest_palindromic_substring.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+uint32_t test01();
+uint32_t test02();
+uint32_t test03();
+
+int main(void)
+{
+    int ret = 0;
+    
+    
+    test01();
+
+    return ret;
+}
+
+uint32_t test01()
+{
+    uint32_t ret = 0;
+
+    
+    return ret;
+}
