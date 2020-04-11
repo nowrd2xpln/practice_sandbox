9b0c72271e35300457bf3d82838a28dafdf0f009
nowrd2xpln <allen.mamaril@gmail.com>
2020-04-11 16:07:22 -0700

    Add C++/Python Hello World templates

---
+++ leetcode/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int test01();
+int test02();
+int test03();
+
+int main()
+{
+    int ret = 0;
+
+    std::cout << "Hello World!\n" << std::endl;    
+        
+    test01();
+
+    return ret;
+}
+
+int test01()
+{
+    int ret = 0;
+
+    
+    return ret;
+}
